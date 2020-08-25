5a9c79d73e61282320d3daaf5cd5615290a38f83
lewis dixon <lewis.dixon@live.co.uk>
2020-08-25 21:14:36 +0100

    changed version number

--- swMesh2XML/swMesh2XML/MainWindow.xaml.cs
+++ swMesh2XML/swMesh2XML/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace swMesh2XML
         public MainWindow()
         {
             InitializeComponent();
-            GlobalVar.version = "v0.1.6-beta-m1";
+            GlobalVar.version = "v0.1.7-beta";
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
